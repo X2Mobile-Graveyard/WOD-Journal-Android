6c37c15a10073c0bae23d3530a223d92f990f977
Modis Laszlo <laszlo.modis@x2mobile.net>
2017-05-15 11:31:11 +0300

    Fixing filterable adapter

--- app/src/main/java/com/x2mobile/wodjar/ui/adapter/base/FilterableBaseAdapter.kt
+++ app/src/main/java/com/x2mobile/wodjar/ui/adapter/base/FilterableBaseAdapter.kt
@@ -1,22 +1,26 @@
 package com.x2mobile.wodjar.ui.adapter.base
 
-import android.text.TextUtils
 import com.x2mobile.wodjar.data.model.base.Filterable
 
 abstract class FilterableBaseAdapter<T : Filterable, VH : BaseViewHolder<T>> : BaseAdapter<T, VH>() {
 
-    private var originalItems: MutableList<T>? = null
+    private var originalItems: List<T>? = null
+
+    private var query: String = ""
+
+    override fun setItems(items: MutableList<T>?) {
+        originalItems = items
+        super.setItems(originalItems?.filter { it.matches(query) }?.toMutableList())
+    }
+
+    override fun clearItems() {
+        super.clearItems()
+        originalItems = null
+    }
 
     fun filter(query: String) {
-        if (TextUtils.isEmpty(query)) {
-            setItems(originalItems!!)
-            originalItems = null
-        } else {
-            if (originalItems == null) {
-                originalItems = getItems()
-            }
-            setItems(originalItems!!.filter { it.matches(query) }.toMutableList())
-        }
+        this.query = query
+        super.setItems(originalItems?.filter { it.matches(query) }?.toMutableList())
     }
 
 }
