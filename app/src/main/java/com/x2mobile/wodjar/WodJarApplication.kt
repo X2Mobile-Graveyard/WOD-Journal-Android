e76869d482c1b359728028143aba74fc4b7d0d9a
Modis Laszlo <laszlo.modis@x2mobile.net>
2017-06-14 10:02:21 +0300

    Updating eventbus configuration

--- app/src/main/java/com/x2mobile/wodjar/WodJarApplication.kt
+++ app/src/main/java/com/x2mobile/wodjar/WodJarApplication.kt
@@ -3,12 +3,15 @@ package com.x2mobile.wodjar
 import android.app.Application
 import android.content.Context
 import com.facebook.stetho.Stetho
+import org.greenrobot.eventbus.EventBus
 
 class WodJarApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         INSTANCE = this
+
+        EventBus.builder().throwSubscriberException(BuildConfig.DEBUG).installDefaultEventBus()
 
         if (BuildConfig.DEBUG) {
             Stetho.initializeWithDefaults(this)
