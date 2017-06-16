85ce2602254f2c99980f94869888e4f9d5660c18
Modis Laszlo <laszlo.modis@x2mobile.net>
2017-06-16 11:58:33 +0300

    Adding fabric

--- app/src/main/java/com/x2mobile/wodjar/WodJarApplication.kt
+++ app/src/main/java/com/x2mobile/wodjar/WodJarApplication.kt
@@ -4,12 +4,18 @@ import android.app.Application
 import android.content.Context
 import com.facebook.stetho.Stetho
 import org.greenrobot.eventbus.EventBus
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.core.CrashlyticsCore
+import io.fabric.sdk.android.Fabric
 
 class WodJarApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         INSTANCE = this
+
+        val core = CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build()
+        Fabric.with(this, Crashlytics.Builder().core(core).build())
 
         EventBus.builder().throwSubscriberException(BuildConfig.DEBUG).installDefaultEventBus()
 
